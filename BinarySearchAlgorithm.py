f7abf328a862a0845a9b21756c45ca2d4637294c
BielAltimira <66877537+BielAltimira@users.noreply.github.com>
2020-09-01 15:09:03 +0200

    Update BinarySearchAlgorithm.py

--- BinarySearchAlgorithm.py
+++ BinarySearchAlgorithm.py
@@ -1,19 +1,19 @@
-def BinaryAlgorithm(Llista, left, right, objectiu):
-    while (left <= right):
-        centre = int(left +(right - left) / 2)
-        if ( objectiu == Llista[centre]):
-            return ("The number is {}".format(Llista[centre]))
-        if (Llista[centre] > objectiu):
-                right = centre - 1
+def BinarySearchAlgorithm(List, Left, Right, Objective):
+    while (Left <= Right):
+        Center = int(Left +(Right - Left) / 2)
+        if (objective == List[Center]):
+            return ("The number is {}".format(List[Center]))
+        if (List[Center] > Objective):
+                Right = Center - 1
         else:
             left = centre + 1 
     return ("Unable to find the selected number")
 
-Llista = [4, 12, 25, 39, 40, 47]
-Objectiu = 25
-Mida = len(Llista)
+List = [4, 12, 25, 39, 40, 47]
+Objective = 25
+Length = len(Llista)
 
-resultat = BinaryAlgorithm(Llista, 0, Mida - 1, Objectiu)
+Result = BinarySearchAlgorithm(List, 0, Length - 1, Objective)
 
 
-print(resultat)
+print(Result)
